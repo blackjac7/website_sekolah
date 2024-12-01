34ec6bb02f31293463b74a1a4573be99d545d02b
blackjac7 <febriansyah705@gmail.com>
2024-12-01 22:51:23 +0700

    dynamic size for image hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,20 +18,32 @@ const Home = () => {
 
   const heroSlides = [
     {
-      image:
-        "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1733055889/hero1_qjwkk1.webp",
+      image: {
+        small:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_512/q_auto/f_auto/v1733055889/hero1_qjwkk1.webp",
+        medium:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_1024/q_auto/f_auto/v1733055889/hero1_qjwkk1.webp",
+      },
       title: "Selamat Datang di SMP IP Yakin Jakarta",
       subtitle: "Membentuk Generasi Unggul, Berakhlak, dan Berprestasi",
     },
     {
-      image:
-        "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1733055889/hero2_oa2prx.webp",
+      image: {
+        small:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_512/q_auto/f_auto/v1733055889/hero2_oa2prx.webp",
+        medium:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_1024/q_auto/f_auto/v1733055889/hero2_oa2prx.webp",
+      },
       title: "Fasilitas Pembelajaran Modern",
       subtitle: "Mendukung Pengembangan Potensi Siswa",
     },
     {
-      image:
-        "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1733055884/hero3_gigw1x.webp",
+      image: {
+        small:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_512/q_auto/f_auto/v1733055884/hero3_gigw1x.webp",
+        medium:
+          "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_1024/q_auto/f_auto/v1733055884/hero3_gigw1x.webp",
+      },
       title: "Prestasi Membanggakan",
       subtitle: "Raih Masa Depan Cemerlang Bersama Kami",
     },
@@ -58,9 +70,12 @@ const Home = () => {
               }`}
             >
               <img
-                src={slide.image}
+                src={slide.image.medium}
                 alt={slide.title}
                 className="w-full h-full object-cover"
+                crossOrigin="anonymous"
+                srcSet={`${slide.image.small} 512w, ${slide.image.medium} 1024w`}
+                sizes="(max-width: 600px) 512px, (max-width: 1024px) 1024px, 2048px"
               />
               <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-black/50" />
             </div>
