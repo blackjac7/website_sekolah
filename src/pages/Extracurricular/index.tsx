443ac223e4d4291497ab6593786a5a33254fb6ec
blackjac7 <febriansyah705@gmail.com>
2024-12-03 08:04:57 +0700

    add image rohkris

--- src/pages/Extracurricular/index.tsx
+++ src/pages/Extracurricular/index.tsx
@@ -20,7 +20,7 @@ const activities: ExtracurricularActivity[] = [
       "Pengembangan bakat musik melalui pembelajaran alat musik dan paduan suara.",
     schedule: "Sabtu, 07:30 - 09:00 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732802973/Seni_Musik_qnbwpd.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732802973/Seni_Musik_qnbwpd.webp",
   },
   {
     id: "3",
@@ -29,7 +29,7 @@ const activities: ExtracurricularActivity[] = [
       "Mempelajari dan melestarikan berbagai tarian tradisional dan modern Indonesia.",
     schedule: "Rabu, 13:00 - 15:00 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_1024/q_auto/f_auto/v1732791235/seni_tari_1_cb7rgy.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My%20Logo/w_1024/q_auto/f_auto/v1732791235/seni_tari_1_cb7rgy.webp",
   },
   {
     id: "4",
@@ -38,7 +38,7 @@ const activities: ExtracurricularActivity[] = [
       "Pengembangan seni musik islami melalui pembelajaran alat musik rebana dan vocal.",
     schedule: "Rabu, 13:00 - 15:00 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732803309/hadroh_11_g8qlsr.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732803309/hadroh_11_g8qlsr.webp",
   },
   {
     id: "5",
@@ -47,7 +47,7 @@ const activities: ExtracurricularActivity[] = [
       "Kegiatan pembinaan keagamaan Islam untuk meningkatkan pemahaman dan praktik keislaman.",
     schedule: ["Selasa, 13:00 - 14:30 WIB", "Sabtu, 09:30 - 10:30 WIB"],
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804810/rohis_dtzeef.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804810/rohis_dtzeef.webp",
   },
   {
     id: "6",
@@ -56,7 +56,7 @@ const activities: ExtracurricularActivity[] = [
       "Kegiatan pembinaan keagamaan Kristen untuk meningkatkan pemahaman dan praktik kekristenan.",
     schedule: "Jumat, 11:00 - 12:30 WIB",
     image:
-      "https://images.unsplash.com/photo-1480185660311-81671f39489c??ixlib=rb-4.0.3",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1733187839/rohkris_xf2im9.webp",
   },
   {
     id: "7",
@@ -65,7 +65,7 @@ const activities: ExtracurricularActivity[] = [
       "Kegiatan yang fokus pada latihan baris-berbaris dan upacara bendera.",
     schedule: "Jum'at, 13:00 - 15:00 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804631/paskibra_v3htds.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804631/paskibra_v3htds.webp",
   },
   {
     id: "8",
@@ -74,7 +74,7 @@ const activities: ExtracurricularActivity[] = [
       "Latihan futsal untuk mengembangkan kemampuan olahraga dan sportivitas.",
     schedule: "Sabtu, 09:00 - 11:30 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732802973/Futsal_hghnis.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732802973/Futsal_hghnis.webp",
   },
   {
     id: "9",
@@ -83,7 +83,7 @@ const activities: ExtracurricularActivity[] = [
       "Seni bela diri tradisional Indonesia yang mengembangkan kemampuan fisik dan mental.",
     schedule: "Sabtu, 06:00 - 07:00 WIB",
     image:
-      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804631/silat_a4ly8k.jpg",
+      "https://res.cloudinary.com/dvnyimxmi/image/upload/t_My Logo/w_1024/q_auto/f_auto/v1732804631/silat_a4ly8k.webp",
   },
 ];
 
